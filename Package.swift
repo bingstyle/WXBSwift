3bc914dbb2585a3a88e810dc83652342ec95d579
WeiXinbing <183292352@qq.com>
2020-08-17 17:05:50 +0800

    update

--- Package.swift
+++ Package.swift
@@ -26,7 +26,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "WXBSwift",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"
+        ),
         .testTarget(
             name: "WXBSwiftTests",
             dependencies: ["WXBSwift"]),
